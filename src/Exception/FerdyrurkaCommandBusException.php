76e9200005afeed24166304660fa87b36bb965cd
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-12-29 18:06:09 +0100

    Added FerdyrurkaCommandBusException

---
+++ src/Exception/FerdyrurkaCommandBusException.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright (c) 2018-2018 Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
+ *
+ * For the full copyright and license information, please view the
+ * https://github.com/ferdyrurka/command-bus-symfony-bundle/blob/master/LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Ferdyrurka\CommandBus\Exception;
+
+use \Exception;
+
+/**
+ * Class FerdyrurkaCommandBusException
+ * @package Ferdyrurka\CommandBus\Exception
+ */
+class FerdyrurkaCommandBusException extends Exception
+{
+}
