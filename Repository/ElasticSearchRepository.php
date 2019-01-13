63aa45fccbcae154deb22af175b9c864f03d1821
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-01-13 16:26:46 +0100

    Added ElasticSerchRepository

---
+++ Repository/ElasticSearchRepository.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright (c) 2018-2018 Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
+ *
+ * For the full copyright and license information, please view the
+ * https://github.com/ferdyrurka/command-bus-symfony-bundle/blob/master/LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Ferdyrurka\CommandBus\Repository;
+
+use Elasticsearch\Client;
+use Ferdyrurka\CommandBus\Entity\Warn;
+use Ferdyrurka\CommandBus\Manager\ElasticSearchManager;
+
+/**
+ * Class ElasticSearchRepository
+ * @package Ferdyrurka\CommandBus\Repository
+ */
+class ElasticSearchRepository implements ElasticSearchRepositoryInterface, RepositoryInterface
+{
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * @var string
+     */
+    protected $index;
+
+    /**
+     * ElasticSearchRepository constructor.
+     * @param ElasticSearchManager $elasticSearchManager
+     */
+    public function __construct(ElasticSearchManager $elasticSearchManager)
+    {
+        $this->index = $elasticSearchManager->getIndex();
+        $this->client = $elasticSearchManager->getManager();
+    }
+
+    /**
+     * @param Warn $warn
+     */
+    public function create(Warn $warn): void
+    {
+        $this->client->create([
+            'index' => $this->index,
+            'type' => 'command-bus',
+            'body' => [
+                'exceptionTime' => $warn->getExceptionTime(),
+                'exception' => $warn->getException(),
+                'message' => $warn->getMessage(),
+                'line' => $warn->getLine(),
+                'handler' => $warn->getHandler(),
+                'command' => $warn->getCommand()
+            ]
+        ]);
+    }
+
+}
