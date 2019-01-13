f21f6fe3437049cbe39483a9a1541349de390c69
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-01-13 16:28:46 +0100

    CommandBusInterface

---
+++ CommandBusInterface.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright (c) 2018-2018 Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
+ *
+ * For the full copyright and license information, please view the
+ * https://github.com/ferdyrurka/command-bus-symfony-bundle/blob/master/LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Ferdyrurka\CommandBus;
+
+use Ferdyrurka\CommandBus\Command\CommandInterface;
+
+/**
+ * Interface CommandBusInterface
+ * @package Ferdyrurka\CommandBus
+ */
+interface CommandBusInterface
+{
+    /**
+     * @param CommandInterface $command
+     */
+    public function handle(CommandInterface $command): void;
+}
