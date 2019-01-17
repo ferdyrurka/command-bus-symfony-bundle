9e4ae010dfb0aeb208e5351cc32822c0939398c1
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-01-17 18:23:27 +0100

    Delete fix path to configuration

--- DependencyInjection/CommandBusSymfonyExtension.php
+++ DependencyInjection/CommandBusSymfonyExtension.php
@@ -37,7 +37,7 @@ final class CommandBusSymfonyExtension extends Extension
         if ((bool) $config['save_statistic_handler']) {
             $loader = new YamlFileLoader(
                 $container,
-                new FileLocator(__DIR__ . '../Resources/config')
+                new FileLocator(__DIR__ . '/../Resources/config')
             );
 
             $loader->load($config['database_type'] . '.yaml');
