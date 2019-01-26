120b62602d90da51824c05928c916dec81266d70
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-01-26 13:25:37 +0100

    Added ViewObjectInterface, QueryBusInterface and QueryHandlerInterface

---
+++ Query/Handler/QueryHandlerInterface.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright (c) 2018-2019 Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
+ *
+ * For the full copyright and license information, please view the
+ * https://github.com/ferdyrurka/command-bus-symfony-bundle/blob/master/LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Ferdyrurka\CommandBus\Query;
+
+use Ferdyrurka\CommandBus\Query\Command\QueryCommandInterface;
+use Ferdyrurka\CommandBus\Query\ViewObject\ViewObjectInterface;
+
+/**
+ * Interface QueryHandlerInterface
+ * @package Ferdyrurka\CommandBus\Query
+ */
+interface QueryHandlerInterface
+{
+    /**
+     * @param QueryCommandInterface $queryCommand
+     * @return ViewObjectInterface
+     */
+    public function handle(QueryCommandInterface $queryCommand): ViewObjectInterface;
+}
