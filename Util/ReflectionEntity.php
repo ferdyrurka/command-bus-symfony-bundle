f132d2885ed8ad1cfb25046dc095a9d53f5bb5d5
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2019-02-10 09:53:26 +0100

    ReflectionEntity PSR2 corrected

--- Util/ReflectionEntity.php
+++ Util/ReflectionEntity.php
@@ -45,11 +45,11 @@ final class ReflectionEntity implements ReflectionEntityInterface
         foreach ($reflection->getMethods() as $method) {
             $nameMethod = $method->getName();
 
-            if (strpos($nameMethod,'get') === false) {
+            if (strpos($nameMethod, 'get') === false) {
                 continue;
             }
 
-            $gettersInArray[lcfirst(str_ireplace('get','', $nameMethod))] = $this->entity->$nameMethod();
+            $gettersInArray[lcfirst(str_ireplace('get', '', $nameMethod))] = $this->entity->$nameMethod();
         }
 
         return $gettersInArray;
